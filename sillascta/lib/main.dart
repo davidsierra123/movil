6a8471cc8da1a81c5093f12f60f78f0772bdc483
davidsierra123 <jdbolivar40@misena.edu.co>
2023-07-25 21:10:48 -0500

    siu

--- sillascta/lib/main.dart
+++ sillascta/lib/main.dart
@@ -9,23 +9,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Column(
-       mainAxisAlignment: MainAxisAlignment.center,
-             children: [
-               Text(
-                 '¡Hola, mundo!',
-                 style: TextStyle(fontSize: 24.0),
-               ),
-               ElevatedButton(
-                 onPressed: () {
-                   // Aquí puedes colocar el código que se ejecutará cuando se presione el botón.
-                   print('¡Botón presionado!');
-                 },
-                 child: Text('Ingresar'),
-               ),
-             ],
-          ),
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.asset(
+              'assets/sillas.png',
+              fit: BoxFit.cover,
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Comprar',
+                    style: TextStyle(fontSize: 21.0,fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'Bienvenido, Aqui podrás Tapizar tus Sillas',
+                    style: TextStyle(fontSize: 21.0, fontWeight: FontWeight.bold ),
+                    textAlign: TextAlign.end,
+                  ),
+                  Text(
+                    'Alquilar',
+                    style: TextStyle(fontSize: 21.0, fontWeight: FontWeight.bold ),
+                    textAlign: TextAlign.end,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Aquí puedes colocar el código que se ejecutará cuando se presione el botón.
+                      print('¡Botón presionado!');
+                    },
+                    child: Text('Ingresar'),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
